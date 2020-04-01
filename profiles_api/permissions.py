159e9e19e9eed2de6f5923d1c7cf9157cefc129e
SteamedGit <timothyhitge@gmail.com>
2020-04-01 20:35:39 +0200

    Added user profile feed API

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -1,6 +1,7 @@
 from rest_framework import permissions
 
 
+
 class UpdateOwnProfile(permissions.BasePermission):
     """Allow users to edit their own profile"""
 
@@ -10,3 +11,14 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
         
         return obj.id == request.user.id #does the id of the user making the request match the id of the user whose profile they want to modify
+
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """Allow users to update their own status"""
+
+    def has_object_permission(self, request, view, obj):
+        """Check the user is trying to update their own status"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user_profile.id == request.user.id 
